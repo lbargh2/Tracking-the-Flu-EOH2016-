7aaf110b6390ca5516fd921b0cfdc520e9b8c276
dikshantpradhan <dikshantpradhan@gmail.com>
2015-12-25 17:14:07 -0600

    updated BasicTweetPull to be able to write to json

--- BasicTweetPull.py
+++ BasicTweetPull.py
@@ -3,81 +3,108 @@ from tweepy.streaming import StreamListener
 from tweepy import OAuthHandler
 from tweepy import Stream
 
-import json
+import json, datetime, time
 
-def PoliticalTweetStream(party, searchTerms, state, locationCoordinates, tweetCount):
+import tweepy
+
+def print_tweets(api, filter, start_date, end_date, region, count):
+
+    page = 1
+    ct = 0
+    deadend = False
+    while True:
+        # run api search
+        tweets = tweepy.Cursor(api.search, q=filter, lang="en", locations=region,since=start_date, until=end_date).items(count)
+
+        for tweet in tweets:
+            #if (datetime.datetime.now() - tweet.created_at).days < 5: # example time stamp: 2015-11-24 02:13:33 or 2015-11-01
+            #    #Do processing here:
+            #    #print tweet.geocode
+            print tweet.text.encode("utf-8")
+            ct = ct + 1
+
+            # exit clause
+            if ct == count:
+                return
+            #else:
+            #    deadend = True
+            #    return
+        if not deadend:
+            page+=1
+            time.sleep(500)
+
+def write_tweets_to_json(api, filter, start_date, end_date, region, count, json_file):
+
+    page = 1
+    ct = 0
+
+    list_of_tweets = []
+
+    deadend = False
+    while True:
+        # run api search
+        tweets = tweepy.Cursor(api.search, q=filter, lang="en", locations=region,since=start_date, until=end_date).items(count)
+
+        for tweet in tweets:
+            #if (datetime.datetime.now() - tweet.created_at).days < 5: # example time stamp: 2015-11-24 02:13:33 or 2015-11-01
+            #    #Do processing here:
+            #    #print tweet.geocode
+
+            list_of_tweets.append(tweet.text.encode("utf-8"))
+
+            #with open('tweets.json', 'a') as f:
+            #     json.dump(tweet.text.encode("utf-8"), f)
+            ct = ct + 1
+
+            # exit clause
+            if ct == count:
+                with open(json_file, 'w') as f:
+                     json.dump(list_of_tweets, f)
+                return
+            #else:
+            #    deadend = True
+            #    return
+        if not deadend:
+            page+=1
+            time.sleep(500)
+
+
+def main():
+
+    # API stuff
     access_token = "97325888-4TiheE2EDlLRrRd0i3RyL99uIRMIjqOgMJilVWxsO"
     access_token_secret = "pHpBZnMavx1lTGA1UnS6Uzs6VvLJuwIBP7ExYXqbozoqg"
     consumer_key = "BuAsYJarW7WleTg0PrsvrrF2O"
     consumer_secret = "REOFH6Ue0vewobC1WILncfQrweqPIHc94JTjCBh3iuPhkckYPz"
-
     auth = OAuthHandler(consumer_key, consumer_secret)
     auth.set_access_token(access_token, access_token_secret)
+    api = tweepy.API(auth)
 
-    class StdOutListener(StreamListener):
-
-        def __init__(self, api=None):
-            super(StdOutListener, self).__init__()
-            self.num_tweets = 0
-
-        def on_data(self, data):
-            #record = {'Text': status.text, 'Created At': status.created_at}
-            #print record #See Tweepy documentation to learn how to access other fields
-            self.num_tweets += 1
-            if self.num_tweets <= tweetCount:
-                decoded = json.loads(data)
-                fileTitle = state + '_' + party + '_tweets.txt'
-                tweet = '@%s: %s' % (decoded['user']['screen_name'], decoded['text'].encode('ascii', 'ignore'))
-                with open(fileTitle, 'a+') as f:
-                    f.write(tweet + '\n')
-                return True
-            else:
-                return False
-
-        def on_error(self, status):
-            print 'Error on status', status
-
-        def on_limit(self, status):
-            print 'Limit threshold exceeded', status
-
-        def on_timeout(self, status):
-            print 'Stream disconnected; continuing...'
-
-
-    stream = Stream(auth, StdOutListener())
-    stream.filter(track=searchTerms, locations = locationCoordinates) #looking at New York City atm
-
-def main():
-
-    # number of tweets per state
-    tweetCt = 5
-
-    # open file with state info and save into array
-
-    file = open("states.txt")
-    states = file.readlines()
+    # filter words (array named "filter")
+    file = open("filter.txt") # read in filter words from file named "filter.txt"; alter that file or just write it in here
+    filter = file.readlines()
     file.close()
+    for x in xrange(0, len(filter)):
+        filter[x] = filter[x].strip()
 
-    # run stream for each state based on array
+    # dates to search within
+    start_date = "2015-11-01" # year-mo-dy format
+    end_date = "2015-12-20"
 
-    for x in range (0, len(states)):
-        states[x] = states[x].strip()
-        if x%5 == 0:
-            state = states[x]
-        if x%5 == 1:
-            lat1 = float(states[x])
-        if x%5 == 2:
-            long1 = float(states[x])
-        if x%5 == 3:
-            lat2 = float(states[x])
-        if x%5 == 4:
-            long2 = float(states[x])
-            locationCoord = [long1,lat1,long2,lat2]
+    # area of search
+    region = [41.65,-79.67,44.91,-73.39] # set using coordinates (I think this is the state of New York atm)
 
-            # run stream after the data for each state has been collected for the array
-            #republican run
-            PoliticalTweetStream('republican', ['republican', 'Jeb Bush', 'Donald Trump'], state, locationCoord, tweetCt)
-            #democrat run
-            PoliticalTweetStream('democrat', ['democrat', 'Hilary Clinton', 'Bernie Sanders'], state, locationCoord, tweetCt)
+    # desired number of tweets
+    count = 20
+
+    # json file to write to (if writing to file)
+    json_file = 'tweets.json'
+
+    ''' run function '''
+    #print_tweets(api, filter, start_date, end_date, region, count)
+    write_tweets_to_json(api, filter, start_date, end_date, region, count, 'tweets.json')
 
 main()
+
+# Be aware that the program will not end after displaying the desired set of tweets at the moment.
+# You will need to Ctrl-C or whatever manually for the time being
